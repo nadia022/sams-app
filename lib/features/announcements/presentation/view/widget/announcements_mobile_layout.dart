9f07f8829621140ccbf3fe29eb018fdfe4bc141b
Nadia <nadiaashraff220@gmail.com>
2026-04-07 23:04:38 +0200

    feat(announcement): enhance announcements layout for mobile and web with dynamic content display

--- lib/features/announcements/presentation/view/widget/announcements_mobile_layout.dart
+++ lib/features/announcements/presentation/view/widget/announcements_mobile_layout.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:sams_app/core/models/main_card_model.dart';
+import 'package:sams_app/core/utils/assets/app_images.dart';
+import 'package:sams_app/core/utils/colors/app_colors.dart';
+import 'package:sams_app/core/utils/styles/app_styles.dart';
+import 'package:sams_app/core/widgets/mobile/mobile_main_card.dart';
 
 class AnnouncementsMobileLayout extends StatelessWidget {
   const AnnouncementsMobileLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Announcements mobile TabView'));
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(height: 8),
+        Text(
+          'Announcements',
+          style: AppStyles.mobileTitleMediumSb.copyWith(
+            color: AppColors.primaryDarkHover,
+          ),
+        ),
+        const SizedBox(height: 8),
+        Expanded(
+          child: ListView.builder(
+            itemCount: 5,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.only(bottom: 12),
+                child: MobileMainCard(
+                  cardModel: MainCardModel(
+                    title: 'Makeup quiz',
+                    description:
+                        'Chapter 1: Database Fundamentals and theories',
+                    image: AppImages.imagesAnnouncementCard,
+                    onTap: () {},
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ],
+    );
   }
 }
--- lib/features/announcements/presentation/view/widget/announcements_web_layout.dart
+++ lib/features/announcements/presentation/view/widget/announcements_web_layout.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:sams_app/core/models/main_card_model.dart';
+import 'package:sams_app/core/utils/assets/app_images.dart';
+import 'package:sams_app/core/utils/colors/app_colors.dart';
+import 'package:sams_app/core/utils/styles/app_styles.dart';
+import 'package:sams_app/core/widgets/web/web_main_card.dart';
 
 //! Materials_web_layout.dart
 class AnnouncementsWebLayout extends StatelessWidget {
@@ -6,6 +11,42 @@ class AnnouncementsWebLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Announcements web TabView'));
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(40, 12, 40, 0),
+      child: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Text(
+              'Announcements',
+              style: AppStyles.webTitleMediumSb.copyWith(
+                color: AppColors.primaryDarkHover,
+              ),
+            ),
+          ),
+          const SliverToBoxAdapter(child: SizedBox(height: 16)),
+          SliverGrid.builder(
+            itemCount: 6,
+            gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 350,
+              mainAxisSpacing: 16,
+              crossAxisSpacing: 16,
+              mainAxisExtent: 220,
+              childAspectRatio: 301/240
+            ),
+            itemBuilder: (context, index) {
+              return WebMainCard(
+                model: MainCardModel(
+                  title: 'Makeup quiz',
+                  description: 'Chapter 1: Database Fundamentals and theories',
+                  image: AppImages.imagesAnnouncementCard,
+                  onTap: () {},
+                ),
+              );
+            },
+          ),
+          const SliverToBoxAdapter(child: SizedBox(height: 12)),
+        ],
+      ),
+    );
   }
 }
