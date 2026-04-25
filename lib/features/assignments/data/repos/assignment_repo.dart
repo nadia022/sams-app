cb581e16384be72b2239d5027d23a799e23cc8df
Muhmd Mustafa <muhmdmustafa10@gmail.com>
2026-04-25 14:53:19 +0300

    feat(assignment): enhance AssignmentRepo with addItemsToAssignment and uploadAssignmentFullWorkflow methods for improved file handling.

--- lib/features/assignments/data/repos/assignment_repo.dart
+++ lib/features/assignments/data/repos/assignment_repo.dart
@@ -1,4 +1,5 @@
 import 'package:dartz/dartz.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:sams_app/features/assignments/data/model/assignment_model.dart';
 
 abstract class AssignmentRepo {
@@ -14,11 +15,26 @@ abstract class AssignmentRepo {
     required String assignmentId,
   });
 
-    Future<Either<String, AssignmentModel>> deleteAssignmentItem({
+  Future<Either<String, AssignmentModel>> deleteAssignmentItem({
     required String assignmentId,
     required String itemKey,
   });
 
+  Future<Either<String, AssignmentModel>> addItemsToAssignment({
+    required String assignmentId,
+    required String courseId,
+    required List<XFile> newFiles,
+    required String classworkId,
+  });
 
-
+  Future<Either<String, AssignmentModel>> uploadAssignmentFullWorkflow({
+    required String courseId,
+    required String title,
+    required String description,
+    required String classworkId,
+    required bool enablePlagiarismCheck,
+    required int plagiarismThreshold,
+    required String dueDate,
+    required List<XFile> selectedFiles,
+  });
 }
